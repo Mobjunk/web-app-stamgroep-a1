d9673fbb3b0b5475329e9070e396a6b5dc3feca3
Marc <marc@bekhuis.net>
2021-03-15 14:32:01 +0100

    fuck

---
+++ 360Photos_WebGL/Assets/Scripts/UINavigator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UINavigator : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private Selectable[] selectables;
+    [SerializeField] private KeyCode switchKey = KeyCode.Tab;
+
+    private int selectedIndex = 0;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        for (int i = 0; i < selectables.Length; i++)
+        {
+            if (selectables[i] == eventData.selectedObject)
+            {
+                selectedIndex = i;
+                return;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(switchKey))
+        {
+            if(selectedIndex < selectables.Length - 1)
+            {
+                selectables[selectedIndex + 1].GetComponent<InputField>().OnPointerClick(null);
+            }
+            else
+            {
+                selectables[0].Select();
+            }
+        }
+    }
+}
