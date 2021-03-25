11723662ae2011f9385c8ed9c076e12394168dd0
marc <marc@bekhuis.net>
2021-03-25 13:32:43 +0100

    Quiz open Editor Make database items

--- 360Photos_WebGL/Assets/Scripts/QuizEditorOpen.cs
+++ 360Photos_WebGL/Assets/Scripts/QuizEditorOpen.cs
@@ -2,19 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuizEditorOpen : MonoBehaviour
+public class QuizEditorOpen : WebRequestManager
 {
-    private ButtonManager buttonManager;
+    public override void FinishedResponse()
+    {
+        if (!webResponse.Contains("Succesfully"))
+        {
+            Debug.LogError(webResponse);
+            return;
+        }
+        Utility.UnloadScene("LoggedIn");
+        Utility.SwitchScenes("QuizPanelScene", "360Ruimte");
+    }
 
     public void OpenEditor(string quizID, string quizName, string quizOwner, string quizAcces, string worldID)
     {
-        if (worldID == null)
+        if (worldID.Trim() == "")
         {
-
+            WWWForm form = new WWWForm();
+            form.AddField("ownerID", quizOwner);
+            form.AddField("accesClassID", quizAcces);
+            form.AddField("name", quizName);
+            form.AddField("quizID", quizID);
+            StartCoroutine(PostRequest($"{Utility.action_url}create360World", form));
         }
         else
         {
-
+            Utility.UnloadScene("LoggedIn");
+            Utility.SwitchScenes("QuizPanelScene", "360Ruimte");
         }
     }
 }
